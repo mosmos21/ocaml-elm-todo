c9b4111e76e834609119d5957492452421ddbfd3
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2020-07-24 11:08:17 +0900

    comlete

--- backend/src/main.ml
+++ backend/src/main.ml
@@ -19,12 +19,16 @@ module DbUtil = struct
     let col = Mysql.column result in
     let rec loop = function
       | None -> []
-      | x -> row_parser col x :: loop (Mysql.fetch result)
+      | Some x -> row_parser col x :: loop (Mysql.fetch result)
     in
-    loop (Mysql.fetch result)
-end
+    loop (Mysql.fetch result) 
 
-let dbd = DbUtil.connect_with_tcp ()
+  let dbd = connect_with_tcp ()
+  
+  let exec query = 
+    Logs.info (fun m -> m "[EXEC QUERY] %s" query);
+    Mysql.exec dbd query
+end
 
 module Todo = struct
   module Status = struct
@@ -48,39 +52,62 @@ module Todo = struct
     | 1 -> Status.IN_PROGRESS
     | 2 -> Status.COMPLETE
     | _ -> Status.NOT_START
+
+  let int_of_status = function
+    | Status.NOT_START -> 0
+    | Status.IN_PROGRESS -> 1
+    | Status.COMPLETE -> 2
+
+  let json_of_todo todo =
+    Yojson.Basic.(
+      `Assoc 
+        [ ("id", `Int todo.id)
+        ; ("title", `String todo.title)
+        ; ("content", `String todo.content)
+        ; ("status", `Int (int_of_status todo.status))
+        ; ("created_at", `String todo.created_at)
+        ; ("updated_at", `String todo.updated_at)
+        ]
+    )
+  
+  let json_of_todos todos =
+    Yojson.Basic.(`List (List.map json_of_todo todos))
 end
 
 module TodoRepository = struct
-  let parse_todo col = function
-    | None -> None
-    | Some x ->
-      let open Mysql in
-      Some Todo.(
-        { id         = not_null int2ml (col ~key:"id" ~row: x)
-        ; title      = not_null str2ml (col ~key:"title" ~row: x)
-        ; content    = not_null str2ml (col ~key:"content" ~row: x)
-        ; status     = status_of_int @@ not_null int2ml (col ~key:"status" ~row: x)
-        ; created_at = not_null str2ml (col ~key:"created_at" ~row: x)
-        ; updated_at =  not_null str2ml (col ~key:"updated_at" ~row: x)
-        }
-      )
+  let parse_todo col x =
+    let open Mysql in
+    Todo.(
+      { id         = not_null int2ml (col ~key:"id" ~row: x)
+      ; title      = not_null str2ml (col ~key:"title" ~row: x)
+      ; content    = not_null str2ml (col ~key:"content" ~row: x)
+      ; status     = status_of_int @@ not_null int2ml (col ~key:"status" ~row: x)
+      ; created_at = not_null str2ml (col ~key:"created_at" ~row: x)
+      ; updated_at =  not_null str2ml (col ~key:"updated_at" ~row: x)
+      }
+    )
 
   let select_todos () =
-    let result = Mysql.(exec dbd "select * from todo_app.todo") in
-    DbUtil.parse_rows parse_todo result
+    let result = DbUtil.exec "select * from todo order by id desc" in
+    DbUtil.parse_rows parse_todo result 
 
   let select_todo id =
-    let result = Mysql.exec dbd (sprintf "select * from todo_app.todo where id = %d" id) in
-    let col = Mysql.column result in
-    parse_todo col (Mysql.fetch result)
+    let result = DbUtil.exec (sprintf "select * from todo where id = %d" id) in
+    match Mysql.fetch result with
+      | Some x -> let col = Mysql.column result in Some (parse_todo col x)
+      | None -> None
   
   let insert_todo title content =
-    ignore(Mysql.exec dbd (sprintf "insert into todo_app(title, content) values (%s, %s)" title content))
+    ignore(DbUtil.exec (sprintf "insert into todo(title, content) values (\"%s\", \"%s\")" title content))
+
+  let update_todo_status id status =
+    ignore(DbUtil.exec (sprintf "update todo set status = %d, updated_at = now() where id = %d" status id))
 end
 
 module EchoHandler = struct
-  let echo () =
-    Server.respond_string ~status:`OK ~body:"echo" ()
+  let echo param =
+    let body = sprintf "{ \"value\": \"%s\" }" param in
+    Server.respond_string ~status:`OK ~body ()
 end
 
 module TodoHandler = struct
@@ -89,8 +116,9 @@ module TodoHandler = struct
     | Some (todo: Todo.t) -> Printf.sprintf "[id: %d] %s - %s" todo.id todo.title todo.content
 
   let get_todos () =
-    let todos = List.map string_of_todo (TodoRepository.select_todos ()) |> String.concat "\n" in
-    Server.respond_string ~status:`OK ~body:todos ()
+    let todos = TodoRepository.select_todos () |> Todo.json_of_todos in
+    let body = Yojson.Basic.pretty_to_string todos in
+    Server.respond_string ~status:`OK ~body ()
 
   let get_todo id =
     match TodoRepository.select_todo id with
@@ -99,16 +127,23 @@ module TodoHandler = struct
 
   let create_todo body = Cohttp_lwt.Body.to_string body 
     >|= begin fun body ->
-      let json_body = Yojson.Basic.from_string body in
+    let json_body = Yojson.Basic.from_string body in
       let open Yojson.Basic.Util in
       let title = json_body |> member "title" |> to_string in
       let content = json_body |> member "content" |> to_string in
       TodoRepository.insert_todo title content
     end
     >>= fun () -> 
-      Server.respond_string ~status:`Created ~body:"" ()
+      Server.respond ~status:`Created ~body:`Empty ()
 
-  let update_todo body id = Server.respond_string ~status:`Created ~body:"\n" ()
+  let update_todo body id = Cohttp_lwt.Body.to_string body
+  >|= begin fun body ->
+    let json_body = Yojson.Basic.from_string body in
+    let status = Yojson.Basic.Util.(json_body |> member "status" |> to_int) in
+    TodoRepository.update_todo_status id status
+  end
+  >>= fun () ->
+    Server.respond ~status:`No_content ~body:`Empty ()
 end
   
 (* ******** ******** ******** ******** *)
@@ -118,15 +153,15 @@ let routes body =
   let open Code in
   function
     | `GET -> one_of 
-      [ s "todos" /? nil @--> TodoHandler.get_todos ()
-      ; s "todos" / int /? nil @--> TodoHandler.get_todo
-      ; s "echo" /? nil @--> EchoHandler.echo ()
+      [ s "api" / s "todos" /? nil @--> TodoHandler.get_todos ()
+      ; s "api" / s "todos" / int /? nil @--> TodoHandler.get_todo
+      ; s "api" / s "echo" / str /? nil @--> EchoHandler.echo 
       ]
     | `POST -> one_of
-      [ s "todos" /? nil @--> TodoHandler.create_todo body 
+      [ s "api" / s "todos" /? nil @--> TodoHandler.create_todo body 
       ]
     | `PUT -> one_of
-      [ s "todos" / int /? nil @--> TodoHandler.update_todo body 
+      [ s "api" / s "todos" / int /? nil @--> TodoHandler.update_todo body 
       ]
     | _ -> one_of []
 
@@ -140,4 +175,7 @@ let server =
   in
   Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
 
-let () = Lwt_main.run server
+let () = 
+  Logs.set_reporter (Logs.format_reporter ());
+  Logs.set_level (Some Logs.Info);
+  Lwt_main.run server
---
+++ frontend/src/Main.elm
@@ -0,0 +1,248 @@
+module Main exposing (Model, Msg(..), main, update, view)
+
+import Browser exposing (Document)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http exposing (..)
+import Json.Decode as JD
+import Json.Encode as Encode
+
+
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type RequestStatus
+    = LOADING
+    | SUCCESS
+    | FAILED
+
+
+type alias Todo =
+    { id : Int
+    , title : String
+    , content : String
+    , status : Int
+    , created_at : String
+    , updated_at : String
+    }
+
+
+type alias Model =
+    { todos : List Todo
+    , title : String
+    , content : String
+    , requestStatus : RequestStatus
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model [] "" "" LOADING, getTodos )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- UPDATE
+
+
+type Msg
+    = GotTodos (Result Http.Error (List Todo))
+    | ChangeTitle String
+    | ChangeContent String
+    | Created (Result Http.Error ())
+    | Updated (Result Http.Error ())
+    | ClickSubmit
+    | StartTodo Int Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotTodos result ->
+            case result of
+                Ok todos ->
+                    ( { model | todos = todos, requestStatus = SUCCESS }, Cmd.none )
+
+                Err _ ->
+                    ( { model | requestStatus = FAILED }, Cmd.none )
+
+        ChangeTitle title ->
+            ( { model | title = title }, Cmd.none )
+
+        ChangeContent content ->
+            ( { model | content = content }, Cmd.none )
+
+        Created _ ->
+            ( model, getTodos )
+
+        Updated _ ->
+            ( model, getTodos )
+
+        ClickSubmit ->
+            ( model, createTodo model.title model.content )
+
+        StartTodo id status ->
+            ( model, updateTodoStatus id status )
+
+
+
+-- VIEW
+
+
+view : Model -> Document Msg
+view model =
+    { title = "TODO"
+    , body =
+        [ div
+            [ style "width" "500px"
+            , style "margin" "10px auto"
+            ]
+            [ h1 [] [ text "Todo List" ]
+            , div
+                [ style "border" "1px solid gray"
+                , style "padding" "8px"
+                ]
+                [ textInput "title" "Title" ChangeTitle
+                , textInput "content" "Content" ChangeContent
+                , button
+                    [ style "margin" "8px auto"
+                    , style "width" "90%"
+                    , onClick ClickSubmit
+                    ]
+                    [ text "Submit" ]
+                ]
+            , div [ style "margin-top" "16px" ] (List.map todoCard model.todos)
+            ]
+        ]
+    }
+
+
+textInput : String -> String -> (String -> msg) -> Html msg
+textInput i lbl inputMsg =
+    div []
+        [ label [ for i ] [ text lbl ]
+        , input
+            [ id i
+            , style "display" "block"
+            , style "width" "90%"
+            , style "margin" "0 auto"
+            , onInput inputMsg
+            ]
+            []
+        ]
+
+
+todoCard : Todo -> Html Msg
+todoCard todo =
+    div
+        [ style "margin" "16px 0"
+        , style "border" "1px solid gray"
+        , style "padding" "8px"
+        ]
+        [ h3 [] [ text (todo.title ++ " [" ++ statusFromInt todo.status ++ "]") ]
+        , p [] [ text todo.content ]
+        , div []
+            [ span
+                [ style "font-size" "12px"
+                , style "color" "gray"
+                ]
+                [ text ("created_at: " ++ todo.created_at ++ " / updated_at: " ++ todo.updated_at)
+                ]
+            ]
+        , if todo.status == 0 then
+            button [ onClick (StartTodo todo.id 1) ] [ text "Start" ]
+
+          else if todo.status == 1 then
+            button [ onClick (StartTodo todo.id 2) ] [ text "Complete" ]
+
+          else
+            span [] []
+        ]
+
+
+statusFromInt : Int -> String
+statusFromInt num =
+    case num of
+        0 ->
+            "NOT_STARRT"
+
+        1 ->
+            "IN PROGRESS"
+
+        _ ->
+            "COMPLETE"
+
+
+
+-- HTTP
+
+
+getTodos : Cmd Msg
+getTodos =
+    Http.get
+        { url = "/api/todos"
+        , expect = Http.expectJson GotTodos todoListDecoder
+        }
+
+
+createTodo : String -> String -> Cmd Msg
+createTodo title content =
+    Http.post
+        { url = "/api/todos"
+        , body =
+            jsonBody
+                (Encode.object
+                    [ ( "title", Encode.string title )
+                    , ( "content", Encode.string content )
+                    ]
+                )
+        , expect = Http.expectWhatever Created
+        }
+
+
+updateTodoStatus : Int -> Int -> Cmd Msg
+updateTodoStatus id status =
+    Http.request
+        { method = "PUT"
+        , headers = [ Http.header "Content-Type" "application/json" ]
+        , url = "/api/todos/" ++ String.fromInt id
+        , body = jsonBody (Encode.object [ ( "status", Encode.int status ) ])
+        , expect = Http.expectWhatever Updated
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+todoListDecoder : JD.Decoder (List Todo)
+todoListDecoder =
+    JD.list todoDecoder
+
+
+todoDecoder : JD.Decoder Todo
+todoDecoder =
+    JD.map6 Todo
+        (JD.field "id" JD.int)
+        (JD.field "title" JD.string)
+        (JD.field "content" JD.string)
+        (JD.field "status" JD.int)
+        (JD.field "created_at" JD.string)
+        (JD.field "updated_at" JD.string)
